2f1cf921cd452f36890ac7e1c3e95eb0fb4b9355
Mikko Riihimäki <mikko.riihimaki@suomi24.fi>
2016-04-17 09:14:24 +0300

    Initial Commit

---
+++ TreBus/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TreBus
+//
+//  Created by Mikko Riihimäki on 17.4.2016.
+//  Copyright © 2016 Mikko Riihimäki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
